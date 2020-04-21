94306a18cc1a562b8876ba58f50fa8af72ad08b4
amitsarna <amit.sarna.584@gmail.com>
2020-04-21 22:14:20 +0100

    Updates

---
+++ precache-manifest.6927386aadf9ed249f5f68ff7adf0da8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7a33e6009ae2a052e9cdd25a10b668",
+    "url": "/AddToTen/index.html"
+  },
+  {
+    "revision": "9176e18be4b2f9ef36f7",
+    "url": "/AddToTen/static/css/main.726d6743.chunk.css"
+  },
+  {
+    "revision": "664e303cfed4fe2a9b2a",
+    "url": "/AddToTen/static/js/2.0b42090d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AddToTen/static/js/2.0b42090d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9176e18be4b2f9ef36f7",
+    "url": "/AddToTen/static/js/main.da143b94.chunk.js"
+  },
+  {
+    "revision": "6c2f3e9326932e3e082c",
+    "url": "/AddToTen/static/js/runtime-main.7596e7b0.js"
+  }
+]);
